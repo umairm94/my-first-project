ba884e6b8f081e20ffa63882a05f1de0564c36d9
Umair <umair-m@live.com>
2022-03-18 03:34:01 +0000

    Added crash courses for JS, CSS and HTML

---
+++ CSS crash course/main.js
@@ -0,0 +1,13 @@
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e){
+    e.preventDefault();
+
+    console.log(nameInput.value);
+}
